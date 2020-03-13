5ac67be2e1e99d7ea139ace0f5dd78076845aaad
Danil Konovalov <47856599+danil4905@users.noreply.github.com>
2020-03-13 15:15:38 +0400

    Update main.js
    Create variable streak for apples streak

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ let
     px = ~~(canv.width) / 2, // player X position
     py = ~~(canv.height) / 2, // player Y position
     pw = ph = 20, // player size
-    aw = ah = 24, // apple size
+    aw = ah = 20, // apple size
+    streak = 0, // apple streak
     apples = [], // apples list
     trail = [], // tail elements list (aka trail)
     tail = 10, // tail size (1 for 10)
@@ -26,6 +27,160 @@ function loop() {
     ctx.fillStyle = '#f7e698';
     ctx.fillRect(0, 0, canv.width, canv.height);
 
+    // force speed
+    px += xv;
+    py += yv;
+
+    // teleports
+    if (px > canv.width) { 
+        px = 0; 
+    }
+    if (px + pw < 0) { 
+        px = canv.width; 
+    }
+    if (py + ph < 0) { 
+        py = canv.height; 
+    }
+    if (py > canv.height) { 
+        py = 0; 
+    }
+
+    // paint the snake itself with the tail elements
+    ctx.fillStyle = '#74b572';
+    for (let i = 0; i < trail.length; i++) {
+        ctx.fillStyle = trail[i].color || '#74b572';
+        ctx.fillRect(trail[i].x, trail[i].y, pw, ph);
+    }
+
+    trail.push({ x: px, y: py, color: ctx.fillStyle });
+
+    // limiter
+    if (trail.length > tail) {
+        trail.shift();
+    }
+
+    // eaten
+    if (trail.length > tail) {
+        trail.shift();
+    }
+
+    // self collisions
+    if (trail.length >= tail && gs) {
+        for (let i = trail.length - tailSafeZone; i >= 0; i--) {
+            if (
+                px < (trail[i].x + pw)
+                && px + pw > trail[i].x
+                && py < (trail[i].y + ph)
+                && py + ph > trail[i].y
+            ) {
+                // got collision
+                tail = 10; // cut the tail
+                speed = baseSpeed; // cut the speed (flash nomore lol xD)
+
+                for (let t = 0; t < trail.length; t++) {
+                    // highlight lossed area
+                    trail[t].color = 'red';
+                    if (t >= trail.length - tail) { break; }
+                }
+            }
+        }
+    }
+
+    // paint apples
+    for (let a = 0; a < apples.length; a++) {
+        ctx.fillStyle = apples[a].color;
+        ctx.fillRect(apples[a].x, apples[a].y, aw, ah);
+    }
+
+    // check for snake head collisions with apples
+    for (let a = 0; a < apples.length; a++) {
+        if (
+            px < (apples[a].x + pw)
+            && px + pw > apples[a].x
+            && py < (apples[a].y + ph)
+            && py + ph > apples[a].y
+        ) {
+            // got collision with apple
+            apples.splice(a, 1); // remove this apple from the apples list
+            tail += 10; // add tail length
+            speed += .1; // add some speed
+            spawnApple(); // spawn another apple(-s)
+            break;
+        }
+    }
+}
+
+// apples spawner
+function spawnApple() {
+    let
+        newApple = {
+            x: ~~(Math.random() * canv.width),
+            y: ~~(Math.random() * canv.height),
+            color: 'red'
+        };
+
+    // forbid to spawn near the edges
+    if (
+        (newApple.x < aw || newApple.x > canv.width - aw)
+        ||
+        (newApple.y < ah || newApple.y > canv.height - ah)
+    ) {
+        spawnApple();
+        return;
+    }
+
+    // check for collisions with tail element, so no apple will be spawned in it
+    for (let i = 0; i < tail.length; i++) {
+        if (
+            newApple.x < (trail[i].x + pw)
+            && newApple.x + aw > trail[i].x
+            && newApple.y < (trail[i].y + ph)
+            && newApple.y + ah > trail[i].y
+        ) {
+            // got collision
+            spawnApple();
+            return;
+        }
+    }
+    apples.push(newApple);
+    if (apples.length < 3 && ~~(Math.random() * 1000) > 700) {
+        // 30% chance to spawn one more apple
+        spawnApple();
+    }
+}
+
+// velocity changer (controls)
+function changeDirection(evt) {
+    if (!fkp && [37, 38, 39, 40].indexOf(evt.keyCode) > -1) {
+        setTimeout(function () { gs = true; }, 1000);
+        fkp = true;
+        spawnApple();
+    }
+    if (cooldown) { return false; }
+
+    /*
+      4 directional movement.
+     */
+    if (evt.keyCode == 37 && !(xv > 0)) // left 
+    { 
+        xv = -speed; yv = 0; 
+    }
+    if (evt.keyCode == 38 && !(yv > 0)) // top 
+    { 
+        xv = 0; yv = -speed; 
+    }
+    if (evt.keyCode == 39 && !(xv < 0)) // right 
+    { 
+        xv = speed; yv = 0; 
+    }
+    if (evt.keyCode == 40 && !(yv < 0)) // down arrow
+    { 
+        xv = 0; yv = speed; 
+    }
+
+    cooldown = true;
+    setTimeout(function () { cooldown = false; }, 100);
+}
     // force speed
     px += xv;
     py += yv;
